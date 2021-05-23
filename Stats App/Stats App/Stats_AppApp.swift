41ba3f8020023d57e61ebd2b09827786659c6278
Jerry Wang <jerry@Jerrys-MacBook-Pro.local>
2021-05-23 18:43:26 -0400

    setup

---
+++ Stats App/Stats App/Stats_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stats_AppApp.swift
+//  Stats App
+//
+//  Created by Jerry Wang on 2021-05-09.
+//
+
+import SwiftUI
+
+@main
+struct Stats_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
